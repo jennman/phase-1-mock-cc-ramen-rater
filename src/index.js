917ab946ec789ffea4a5ea4ea3c81c27d42c5053
Jennifer Man <jenniferman702@gmail.com>
2022-09-27 09:53:33 -0400

    commit tree e5b5408f499241d99824268b409d4f50d2e9f59c

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 // write your code here
+const baseUrl = 'http://localhost:3000'
+const ramenUrl = baseUrl + 'ramens'
+
+function addRamen(ramen){
+    const addRamenDiv = document.getElementById('ramens')
+}
+
+// function (getRamenMenu) {
+    console.log(document.getElementById('ramen-menu'))
+// }
